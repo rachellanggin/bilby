4ccac1d7d57151b7f74c39b5488d5a52572c0ecb
rlanggin <rachellanggin@gmail.com>
2025-05-21 14:31:23 -0700

    hmmm

--- bilby/gw/likelihood/multiband.py
+++ bilby/gw/likelihood/multiband.py
@@ -764,8 +764,8 @@ class MBGravitationalWaveTransient(GravitationalWaveTransient):
         
         response_plus, response_cross = interferometer.antenna_response(
             self.parameters['ra'], self.parameters['dec'],
-            self.parameters['geocent_time'], self.parameters['psi'], self.parameters['chirp_mass'],  # should time_ref instead be geocent_time
-            interferometer.frequency_mask)
+            get_whitened_time_series_from_whitened_frequency_series, self.parameters['psi'], self.parameters['chirp_mass'],  # should time_ref instead be geocent_time
+            self.waveform_generator.waveform_arguments['frequencies'])
 
         print('antenna response: ', response_plus, response_cross)
 
