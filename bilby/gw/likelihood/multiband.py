99794e80b85173df425e9f5a93f419a554fa6d8d
rlanggin <rachellanggin@gmail.com>
2025-05-21 13:09:27 -0700

    change which frequency is passed to earth_delay_

--- bilby/gw/likelihood/multiband.py
+++ bilby/gw/likelihood/multiband.py
@@ -539,7 +539,7 @@ class MBGravitationalWaveTransient(GravitationalWaveTransient):
             fddata = np.zeros(N // 2 + 1, dtype=complex)
             fddata[:len(ifo.frequency_domain_strain)][ifo.frequency_mask[:len(fddata)]] += \
                 ifo.frequency_domain_strain[ifo.frequency_mask] / ifo.power_spectral_density_array[ifo.frequency_mask]
-            print('len ffdata (linear coeffs): ', len(ffdata))
+            print('len fddata (linear coeffs): ', len(fddata))
             for b in range(self.number_of_bands):
                 Ks, Ke = self.Ks_Ke[b]
                 windows = self._get_window_sequence(1. / self.durations[b], Ks, Ke - Ks + 1, b)
