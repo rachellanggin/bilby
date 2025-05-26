6815f4d84b55f383c884a48c17864a06698b78ad
rlanggin <rachellanggin@gmail.com>
2025-05-25 18:04:12 -0700

    fix: max_freq

--- bilby/gw/likelihood/multiband.py
+++ bilby/gw/likelihood/multiband.py
@@ -763,10 +763,10 @@ class MBGravitationalWaveTransient(GravitationalWaveTransient):
 
         # Create a matching frequency array
         frequencies = self.banded_frequency_points
-        min_freq = 7.0
+        min_freq = interferometer.strain_data.minimum_frequency
         max_freq = 16.0
-        print(interferometer.strain_data.minimum_frequency)
-        mask = (frequencies >= min_freq) & (frequencies <= threshold_frequency)
+        print(interferometer.strain_data.maximum_frequency)
+        mask = (frequencies >= min_freq) & (frequencies <= max_freq)
         cut_freqs = frequencies[mask]
 
         plus = plus[mask]
