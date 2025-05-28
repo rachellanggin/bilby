fc5efd0fdface78f1c2dd8a5c90ea70f26c4a7a3
rlanggin <rachellanggin@gmail.com>
2025-05-28 13:00:11 -0700

    try start time in interferometer from times_and_frequencies instead of strain data

--- bilby/gw/detector/interferometer.py
+++ bilby/gw/detector/interferometer.py
@@ -36,7 +36,7 @@ class Interferometer(object):
 
     duration = PropertyAccessor('strain_data', 'duration')
     sampling_frequency = PropertyAccessor('strain_data', 'sampling_frequency')
-    start_time = PropertyAccessor('strain_data', 'start_time')
+    start_time = PropertyAccessor('_times_and_frequencies', 'start_time')
     frequency_array = PropertyAccessor('strain_data', 'frequency_array')
     time_array = PropertyAccessor('strain_data', 'time_array')
     minimum_frequency = PropertyAccessor('strain_data', 'minimum_frequency')
