1ca962ed9126fa32125fe97236e6b7f45381d3e6
rlanggin <rachellanggin@gmail.com>
2025-05-30 13:42:46 -0700

    try [self.unique_to_original_frequencies] mask

--- bilby/gw/likelihood/multiband.py
+++ bilby/gw/likelihood/multiband.py
@@ -772,9 +772,9 @@ class MBGravitationalWaveTransient(GravitationalWaveTransient):
         
         strain = np.zeros(len(cut_freqs), dtype=complex) 
         print("waveform polarizations:", len(waveform_polarizations['plus']))
-        # Apply cut_freq mask to the waveform polarizations 
-        plus_cut = waveform_polarizations['plus'][mask]
-        cross_cut = waveform_polarizations['cross'][mask]
+        # Apply [self.unique_to_original_frequencies] mask to the waveform polarizations 
+        plus_cut = waveform_polarizations['plus'][self.unique_to_original_frequencies]
+        cross_cut = waveform_polarizations['cross'][self.unique_to_original_frequencies]
 
         # Get the antenna response using our cut_freqs
         response_plus, response_cross = interferometer.antenna_response(
