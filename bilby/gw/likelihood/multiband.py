1047aef52ceee4f8a69efb315b2306220b737e7b
rlanggin <rachellanggin@gmail.com>
2025-06-30 13:26:07 -0500

    trim strain and linear coeffs to same range

--- bilby/gw/likelihood/multiband.py
+++ bilby/gw/likelihood/multiband.py
@@ -885,11 +885,14 @@ class MBGravitationalWaveTransient(GravitationalWaveTransient):
         #print('strain:', strain)
         # We have to also apply our frequency mask to the linear coeffs so that we can mult. together the cut strain with them
         idxs = np.nonzero(mask)[0]
-        # lin_coeffs_full = self.linear_coeffs[interferometer.name]
-        # lin_coeffs_cut  = lin_coeffs_full[idxs]
-        # d_inner_h = np.conj(np.dot(strain, lin_coeffs_cut))
+        # Trim both arrays to their common minimum length
+        n = min(len(strain[idxs]), len(coeffs))
+        strain_cut = strain[idxs][:n]
+        coeffs_cut = coeffs[:n]
 
-        d_inner_h = np.conj(np.dot(strain[idxs], self.linear_coeffs[interferometer.name]))
+        d_inner_h = np.conj(np.dot(strain_cut, coeffs_cut))
+
+        # d_inner_h = np.conj(np.dot(strain[idxs], self.linear_coeffs[interferometer.name]))
 
         #print("d_inner_h: ", len(d_inner_h))
         # We always linear_interpolate so that we use the first part of our if statement:
