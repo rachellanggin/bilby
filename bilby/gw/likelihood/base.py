a9c7b4a5d7aa94e4f2ee3f43731a1b4b46158a61
rlanggin <rachellanggin@gmail.com>
2025-07-08 16:23:50 -0700

    change to interferometer.parameters

--- bilby/gw/likelihood/base.py
+++ bilby/gw/likelihood/base.py
@@ -712,8 +712,10 @@ class GravitationalWaveTransient(Likelihood):
             Interferometer to compute the response with respect to.
         """
         return interferometer.get_detector_response(
-            signal_polarizations, frequencies=interferometer.strain_data.frequency_array,
-            parameters=self.parameters, raise_error=True)
+            waveform_polarizations=signal_polarizations, 
+            frequencies=interferometer.strain_data.frequency_array,
+            parameters=interferometer.parameters, 
+            raise_error=True)
 
     def generate_phase_sample_from_marginalized_likelihood(
             self, signal_polarizations=None):
