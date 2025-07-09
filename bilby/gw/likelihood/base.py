94872dceda5eac2bc4eae43d268efdb56403786b
rlanggin <rachellanggin@gmail.com>
2025-07-08 17:07:53 -0700

    parameters=self.parameters in compute_full_waveform call to get_detetector_response

--- bilby/gw/likelihood/base.py
+++ bilby/gw/likelihood/base.py
@@ -714,7 +714,7 @@ class GravitationalWaveTransient(Likelihood):
         return interferometer.get_detector_response(
             waveform_polarizations=signal_polarizations, 
             frequencies=interferometer.strain_data.frequency_array,
-            parameters=interferometer.parameters, 
+            parameters=self.parameters, 
             raise_error=True)
 
     def generate_phase_sample_from_marginalized_likelihood(
